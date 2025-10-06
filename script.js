24ad98177e9856ea70e617f9633a88503173b54e
mayalh22 <maya.l.hazarika@gmail.com>
2025-10-06 15:46:58 -0400

    updated to expand on click

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   recipes.forEach(recipe => {
     recipe.addEventListener('click', () => {
-      recipes.forEach(r => r.classList.remove('selected'))
+      recipes.forEach(r => {
+        r.classList.remove('selected')
+        r.querySelectorAll('p').forEach(p => p.style.display = 'none')
+      })
+
       recipe.classList.add('selected')
+      recipe.querySelectorAll('p').forEach(p => p.style.display = 'block')
+
       alert('you selected: ' + recipe.querySelector('h2').textContent)
     })
+
+    recipe.querySelectorAll('p').forEach(p => p.style.display = 'none')
   })
 })
