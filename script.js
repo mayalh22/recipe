03ff6047ef6c402a33c69e74ae53a0774156f5f3
mayalh22 <maya.l.hazarika@gmail.com>
2025-10-06 15:42:40 -0400

    highlighting function added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-
 document.addEventListener('DOMContentLoaded', () => {
   const recipes = document.querySelectorAll('.recipe')
+
   recipes.forEach(recipe => {
     recipe.addEventListener('click', () => {
+      recipes.forEach(r => r.classList.remove('selected'))
+      recipe.classList.add('selected')
       alert('you selected: ' + recipe.querySelector('h2').textContent)
     })
   })
