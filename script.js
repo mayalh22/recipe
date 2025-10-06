3db7f5232f955a13057734d9c18b715df6fd1316
mayalh22 <maya.l.hazarika@gmail.com>
2025-10-06 15:35:28 -0400

    added simple code

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+  const recipes = document.querySelectorAll('.recipe')
+  recipes.forEach(recipe => {
+    recipe.addEventListener('click', () => {
+      alert('you selected: ' + recipe.querySelector('h2').textContent)
+    })
+  })
+})
